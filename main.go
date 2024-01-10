71f4f68607b825baeba2789d6a19a7a8ed04ccb9
MarcoVitangeli <marco_augusto_vitangeli@outlook.com>
2024-01-09 23:54:19 -0300

    added simple window with text grid

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	a := app.New()
+	w := a.NewWindow("Termigor")
+
+	w.Resize(fyne.NewSize(800, 500))
+
+	tg := widget.NewTextGridFromString(`Hola mundo!
+como estan?`)
+
+	cv := w.Canvas()
+
+	cv.SetOnTypedKey(func(ev *fyne.KeyEvent) {
+		switch ev.Name {
+		case fyne.KeyReturn:
+			row := widget.TextGridRow{
+				Cells: []widget.TextGridCell{
+					{
+						Rune:  '0',
+						Style: nil,
+					},
+				},
+				Style: nil,
+			}
+			tg.Rows = append(tg.Rows, row)
+			tg.Refresh()
+		}
+	})
+
+	c := container.NewVBox()
+
+	c.Add(tg)
+
+	w.SetContent(c)
+	w.Show()
+	a.Run()
 }
